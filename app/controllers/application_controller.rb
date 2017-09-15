3f5040009e94b29f1620fb0cc32bb316091773e6
Chase Paules <cpaules@gmail.com>
2017-09-15 14:02:50 +0000

    separated application_controller from games_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,41 +14,4 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  get '/games/new' do
-    erb :'games/create_game'
-  end
-
-  post '/games/new' do
-    if params[:name] != ""
-      @game = Game.create(params)
-      @game.ratings.delete_all
-      redirect to '/'
-    end
-    redirect to '/games/new'
-  end
-
-  get '/games' do
-    @user = current_user(session)
-      if is_logged_in?(session) && @user
-      erb :'/games/games'
-    else
-      redirect to "/login"
-    end
-  end
-
-  get '/games/:id/add' do
-    if is_logged_in?(session)
-      @game = Game.find(params[:id])
-      @user = current_user(session)
-      if @user.games.include?(@game)
-        redirect to '/games'
-      else
-        @user.games << @game
-        redirect to '/games'
-      end
-    else
-      redirect to '/login'
-    end
-  end
-
 end
---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,40 @@
+class GamesController < ApplicationController
+
+  get '/games/new' do
+    erb :'games/create_game'
+  end
+
+  post '/games/new' do
+    if params[:name] != ""
+      @game = Game.create(params)
+      @game.ratings.delete_all
+      redirect to '/'
+    end
+    redirect to '/games/new'
+  end
+
+  get '/games' do
+    @user = current_user(session)
+      if is_logged_in?(session) && @user
+      erb :'/games/games'
+    else
+      redirect to "/login"
+    end
+  end
+
+  get '/games/:id/add' do
+    if is_logged_in?(session)
+      @game = Game.find(params[:id])
+      @user = current_user(session)
+      if @user.games.include?(@game)
+        redirect to '/games'
+      else
+        @user.games << @game
+        redirect to '/games'
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
+end
